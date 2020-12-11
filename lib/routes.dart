c889433eb65f2a2416c5d89895040b5a6d7c0a6d
Etornam <sunumacbright@gmail.com>
2020-12-11 16:56:44 +0000

    added new routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,23 @@
-class Routes {
+import 'package:flutter/material.dart';
+import 'package:muezikfy/views/home/home_view.dart';
+import 'package:muezikfy/views/intro/splash_view.dart';
+import 'package:muezikfy/views/playing/playing_view.dart';
 
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => SplashView());
+      case '/homeView':
+        return MaterialPageRoute(builder: (_) => HomeView());
+      case '/playingView':
+        return MaterialPageRoute(builder: (_) => PlayingView());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+              body: Center(
+                  child: Text('No route defined for ${settings.name}')),
+            ));
+    }
+  }
 }
