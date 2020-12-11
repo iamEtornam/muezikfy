9238a5a6542f4c5e5c42ae3c65b11d4b530e6b49
Etornam <sunumacbright@gmail.com>
2020-12-11 16:55:49 +0000

    added now playing view

---
+++ lib/views/playing/playing_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class PlayingView extends StatefulWidget {
+  PlayingView({Key key}) : super(key: key);
+
+  @override
+  _PlayingViewState createState() => _PlayingViewState();
+}
+
+class _PlayingViewState extends State<PlayingView> {
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'to_playing',
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'PLAYING',
+            style: Theme.of(context)
+                .textTheme
+                .headline6
+                .copyWith(fontWeight: FontWeight.w600),
+          ),
+          actions: [
+            IconButton(icon: Icon(Icons.more_vert), onPressed: (){
+
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
