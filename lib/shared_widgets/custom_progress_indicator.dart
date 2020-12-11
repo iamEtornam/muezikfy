8160e0f3b1bf154298217abbbb71de9940c24038
Etornam <sunumacbright@gmail.com>
2020-12-11 16:55:04 +0000

    added platform custom progress indicators

---
+++ lib/shared_widgets/custom_progress_indicator.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomProgressIndicator extends StatelessWidget {
+  final double size;
+
+  const CustomProgressIndicator({Key key, this.size = 45.0}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Platform.isIOS
+            ? CupertinoActivityIndicator()
+            : SizedBox(
+            width: size, height: size, child: CircularProgressIndicator()));
+  }
+}
