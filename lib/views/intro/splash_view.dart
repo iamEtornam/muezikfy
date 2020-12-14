62fcf6ee07a1727361fec09ca2df872b532b7ee2
Etornam <sunumacbright@gmail.com>
2020-12-14 16:21:28 +0000

    Update splash_view.dart

--- lib/views/intro/splash_view.dart
+++ lib/views/intro/splash_view.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_audio_query/flutter_audio_query.dart';
+import 'package:muezikfy/services/songs_persistence_service.dart';
 import 'package:muezikfy/shared_widgets/custom_progress_indicator.dart';
 
 class SplashView extends StatefulWidget {
@@ -7,13 +10,27 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  final FlutterAudioQuery audioQuery = FlutterAudioQuery();
+  final SongsPersistenceService _songsPersistenceService = SongsPersistenceService();
 
   @override
   void initState() {
-    Future.delayed(Duration(seconds: 5),(){
-      Navigator.pushNamedAndRemoveUntil(context, '/homeView', (route) => false);
-    });
+    loginAuthState();
     super.initState();
+  }
+
+  Future loginAuthState() async {
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    if (_firebaseAuth?.currentUser != null) {
+      List<SongInfo> songs = await audioQuery.getSongs();
+
+      await _songsPersistenceService.insertSongs(songs: songs);
+      Navigator.pushNamedAndRemoveUntil(
+          context, '/homeView', (route) => false);
+    } else {
+      Navigator.pushNamedAndRemoveUntil(
+          context, '/loginView', (route) => false);
+    }
   }
 
   @override
@@ -23,7 +40,9 @@ class _SplashViewState extends State<SplashView> {
         children: [
           Spacer(),
           CustomProgressIndicator(),
-          SizedBox(height: 24,)
+          SizedBox(
+            height: 24,
+          )
         ],
       ),
     );
