25bcdcfefe4c1b17b517311b0e322af3870b4953
iamEtornam <sunumacbright@gmail.com>
2023-12-27 11:31:07 +0000

    Update custom_progress_indicator.dart

--- lib/shared_widgets/custom_progress_indicator.dart
+++ lib/shared_widgets/custom_progress_indicator.dart
@@ -2,17 +2,22 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:muezikfy/utilities/color_schemes.dart';
 
 class CustomProgressIndicator extends StatelessWidget {
   final double size;
 
-  const CustomProgressIndicator({super.key, this.size = 45.0});
+  const CustomProgressIndicator({super.key, this.size = 25.0});
   @override
   Widget build(BuildContext context) {
     return Center(
         child: Platform.isIOS
             ? const CupertinoActivityIndicator()
             : SizedBox(
-            width: size, height: size, child: const CircularProgressIndicator()));
+                width: size,
+                height: size,
+                child: const CircularProgressIndicator(
+                  color: colorMain,
+                )));
   }
 }
