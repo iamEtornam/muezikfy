65fabfb5cc503c6041bd31413a5a368af1afe332
iamEtornam <sunumacbright@gmail.com>
2023-12-27 11:30:58 +0000

    add launcher icons

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -3,6 +3,7 @@ part of 'resources.dart';
 class Images {
   Images._();
 
+  static const String appstore = 'assets/appstore.png';
   static const String avatar = 'assets/avatar.png';
   static const String popSmoke = 'assets/pop_smoke.jpeg';
   static const String popSmokeAlbum = 'assets/pop_smoke_album.png';
